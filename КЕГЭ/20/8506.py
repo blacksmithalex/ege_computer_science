1deb4d358d449a5864b064ef46180e55992df9e0
blacksmithalex <blacksmithalex@mail.ru>
2023-06-07 13:56:13 +0300

    07june23

---
+++ КЕГЭ/20/8506.py
@@ -0,0 +1,16 @@
+def f(x, p):
+    if x >= 55 and p == 4:
+        return True
+    elif x < 55 and p == 4:
+        return False
+    elif x >= 55:
+        return False
+    else:
+        if p % 2 == 1:
+            return f(x + 1, p + 1) or f(x + 4, p + 1) or f(x * 3, p + 1)
+        else:
+            return f(x + 1, p + 1) and f(x + 4, p + 1) and f(x * 3, p + 1)
+
+for S in range(1, 54 + 1):
+    if f(S, 1):
+        print(S)
